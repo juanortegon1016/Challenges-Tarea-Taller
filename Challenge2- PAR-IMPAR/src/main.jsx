4d0e9175bbd097c55caf22e002251664661c28b0
juanortegon1016 <100365995+juanortegon1016@users.noreply.github.com>
2023-03-02 22:24:37 -0500

    Add files via upload

---
+++ Challenge2- PAR-IMPAR/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { Challenge2ParImpar } from './components/Challenge2ParImpar'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+    <Challenge2ParImpar/>
+    <App/>
+
+  </React.StrictMode>,
+)
